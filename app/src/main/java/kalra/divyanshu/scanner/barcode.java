4d00de3014af4257bc02cbfea1abebaf4375d840
Divyanshu Kalra <kalradivyanshu@gmail.com>
2016-05-29 05:09:28 +0530

    Fixed memory issue, now all scans rewrite the last one, saving memory.

--- app/src/main/java/kalra/divyanshu/scanner/barcode.java
+++ app/src/main/java/kalra/divyanshu/scanner/barcode.java
@@ -21,8 +21,6 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class barcode extends AppCompatActivity {
 
@@ -41,8 +39,7 @@ public class barcode extends AppCompatActivity {
 
     private File createImageFile() throws IOException {
         // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
+        String imageFileName = "qrcodescanned";
         File storageDir = Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
