80ddf8a0a933bb652fef99aa020f6bafa98169f5
Divyanshu Kalra <kalradivyanshu@gmail.com>
2016-05-29 04:31:40 +0530

    Added capture image (using android's camera intent). QR code scanner of that image not working yet.

--- app/src/main/java/kalra/divyanshu/scanner/barcode.java
+++ app/src/main/java/kalra/divyanshu/scanner/barcode.java
@@ -1,8 +1,9 @@
 package kalra.divyanshu.scanner;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.SparseArray;
 import android.view.View;
@@ -18,31 +19,55 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 public class barcode extends AppCompatActivity {
 
     Button btn;
+    ImageView myImageView;
+    Bitmap imageBitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.barcode);
         btn = (Button) findViewById(R.id.button);
+        myImageView = (ImageView) findViewById(R.id.imgview);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            myImageView.setImageBitmap(imageBitmap);
+            BarcodeDetector detector =
+                    new BarcodeDetector.Builder(getApplicationContext())
+                            .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                            .build();
+            if(!detector.isOperational()){
+                Toast.makeText(barcode.this, "Could not set up the detector!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Frame frame = new Frame.Builder().setBitmap(imageBitmap).build();
+            SparseArray<Barcode> barcodes = detector.detect(frame);
+            try {
+                Barcode thisCode = barcodes.valueAt(0);
+                TextView txtView = (TextView) findViewById(R.id.txtContent);
+                txtView.setText(thisCode.rawValue);
+            }
+            catch (Exception e) {
+                Toast.makeText(barcode.this, "Could not find QR code!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     public void scanit(View v) {
-        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
-        Bitmap myBitmap = BitmapFactory.decodeResource(
-                getApplicationContext().getResources(),
-                R.drawable.qrcode);
-        myImageView.setImageBitmap(myBitmap);
-        BarcodeDetector detector =
-                new BarcodeDetector.Builder(getApplicationContext())
-                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
-                        .build();
-        if(!detector.isOperational()){
-            Toast.makeText(barcode.this, "Could not set up the detector!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
-        SparseArray<Barcode> barcodes = detector.detect(frame);
-        Barcode thisCode = barcodes.valueAt(0);
-        TextView txtView = (TextView) findViewById(R.id.txtContent);
-        txtView.setText(thisCode.rawValue);
+        dispatchTakePictureIntent();
     }
+
 }
