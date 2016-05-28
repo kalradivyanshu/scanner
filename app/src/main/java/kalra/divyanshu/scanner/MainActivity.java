84b84b4e0ec401b7d03cc653e08c94c1f4bc4958
Divyanshu Kalra <kalradivyanshu@gmail.com>
2016-05-29 03:58:24 +0530

    Added barcode scanner to scan a barcode (Stored in drawable)

--- app/src/main/java/kalra/divyanshu/scanner/MainActivity.java
+++ app/src/main/java/kalra/divyanshu/scanner/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import com.google.android.gms.vision.barcode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(context).build();
     }
 
     protected void startscanner(View view){
         Intent intent = new Intent(this, barcode.class);
         startActivity(intent);
     }
+
+
 }
--- app/src/main/java/kalra/divyanshu/scanner/barcode.java
+++ app/src/main/java/kalra/divyanshu/scanner/barcode.java
@@ -1,13 +1,48 @@
 package kalra.divyanshu.scanner;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class barcode extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.barcode);
+        btn = (Button) findViewById(R.id.button);
+    }
+
+    public void scanit(View v) {
+        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+        Bitmap myBitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.qrcode);
+        myImageView.setImageBitmap(myBitmap);
+        BarcodeDetector detector =
+                new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+        if(!detector.isOperational()){
+            Toast.makeText(barcode.this, "Could not set up the detector!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+        SparseArray<Barcode> barcodes = detector.detect(frame);
+        Barcode thisCode = barcodes.valueAt(0);
+        TextView txtView = (TextView) findViewById(R.id.txtContent);
+        txtView.setText(thisCode.rawValue);
     }
 }
