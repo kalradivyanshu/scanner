70012d4c0c2d6d52b340501f4f06f941a8df19d1
Divyanshu Kalra <kalradivyanshu@gmail.com>
2016-05-29 03:21:13 +0530

    Made barcode activity(basic) and connected it to a button

--- app/src/main/java/kalra/divyanshu/scanner/MainActivity.java
+++ app/src/main/java/kalra/divyanshu/scanner/MainActivity.java
@@ -1,7 +1,10 @@
 package kalra.divyanshu.scanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import com.google.android.gms.vision.barcode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(context).build();
     }
 
-
+    protected void startscanner(View view){
+        Intent intent = new Intent(this, barcode.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/kalra/divyanshu/scanner/barcode.java
@@ -0,0 +1,13 @@
+package kalra.divyanshu.scanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class barcode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.barcode);
+    }
+}
